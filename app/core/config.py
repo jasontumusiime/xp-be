9d98715980e3e451c477920ce67a64794a6ef468
Jason Tumusiime <app.emhug.org@gmail.com>
2026-08-13 01:56:23 +0300

    Make POSTGRES_SERVER and POSTGRES_USER optional, DATABASE_URL takes priority

--- app/core/config.py
+++ app/core/config.py
@@ -50,9 +50,9 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str
     SENTRY_DSN: HttpUrl | None = None
-    POSTGRES_SERVER: str
+    POSTGRES_SERVER: str = ""
     POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str
+    POSTGRES_USER: str = ""
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
     DATABASE_URL: str | None = None
